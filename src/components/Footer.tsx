00c39018e16d02e47e44c2f6d871f6e1a8287196
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-12 13:08:16 -0400

    created footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SiGithub } from 'react-icons/si'
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-row justify-center  bg-emerald-200 w-screen ">
+      <ul className="flex flex-row text-center justify-center ">
+        <li className="flex flex-row align-middle justify-center text-purple-600 hover:scale-125 transition-all duration-300">
+          <SiGithub className="font-bold my-auto" />
+          <a
+            href="https://github.com/sramalho94/portfolio_2023"
+            className="font-bold"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repo
+          </a>
+        </li>
+        <li className="flex flex-row pl-4">
+          <p>Built Using: Next.js, Tailwindcss</p>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
