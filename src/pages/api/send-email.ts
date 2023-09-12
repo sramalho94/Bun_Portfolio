b0c662fac947fae74d58bc931edc597aeed4fca6
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-12 15:40:47 -0400

    created AboutMe component

---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,24 @@
+// pages/api/send-email.ts
+import sgMail, { MailDataRequired } from '@sendgrid/mail'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const msg: MailDataRequired = req.body
+
+    try {
+      await sgMail.send(msg)
+      res.status(200).json({ success: true })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ success: false, error })
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
+}
